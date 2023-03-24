5ae5e748125f48f3926fb40f55bcbae89fa4f8b5
Noah Too <krivahtoo@gmail.com>
2023-03-24 09:45:12 +0300

    fix task 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * print_n - print long int
+ *
+ * @n: number to print.
+ */
+void print_n(int n)
+{
+	if (n >= 10 || n <= -10)
+		print_n(n / 10);
+	if (n < 0)
+		_putchar('0' + (n % 10) * -1);
+	else
+		_putchar('0' + (n % 10));
+}
+
+/**
  * print_number - print number.
  *
  * @n: the number to print.
@@ -9,14 +24,11 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		n = n * -1;
 		_putchar('-');
 	}
 	if (n != 0)
 	{
-		if (n >= 10)
-			print_number(n / 10);
-		_putchar('0' + (n % 10));
+		print_n(n);
 	}
 	else
 	{
