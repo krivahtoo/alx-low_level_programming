794e76aba20b09445f9778175bb79b192357fb7d
Noah Too <krivahtoo@gmail.com>
2023-03-30 00:41:47 +0300

    Task 3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0 = equal, -ve s1 greater, +ve s2 greater
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+			break;
+		i++;
+	}
+	return s1[i] - s2[i];
+}
