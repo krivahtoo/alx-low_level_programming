38657296e100271e268b6082d842dbd12dbbcdde
Noah Too <krivahtoo@gmail.com>
2023-03-30 17:13:35 +0300

    Task 9. Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _abs - get absolute of a number
+ *
+ * @n: number
+ *
+ * Return: absolute number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return ((-1) * n);
+	return (n);
+}
+
+/**
+ * print_number - print a number.
+ *
+ * @n: number to print
+ */
+void print_int(int n)
+{
+	if (n != 0)
+	{
+		if (n / 10 != 0)
+			print_int(n / 10);
+		_putchar('0' + _abs(n % 10));
+	}
+	else
+		_putchar('0');
+}
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+		_putchar('-');
+	print_int(n);
+}
