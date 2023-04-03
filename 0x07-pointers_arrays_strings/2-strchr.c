0d129df1dab24677e069e062df0b67b64dbfd48c
Noah Too <krivahtoo@gmail.com>
2023-04-03 23:14:10 +0300

    Task 2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string.
+ *
+ * @s: string to check
+ * @c: character to check for
+ *
+ * Return: pointer to first or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return s;
+		s++;
+	}
+	return (NULL);
+}
