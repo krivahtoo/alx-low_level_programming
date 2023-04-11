7ad4a64c809a338751f4358ca869ce821a62013b
Noah Too <krivahtoo@gmail.com>
2023-04-12 00:17:19 +0300

    fix: free everything on failure

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,11 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = (int *)malloc(width * sizeof(int));
 		if (arr[i] == NULL)
+		{
+			free(arr[i]);
+			free(arr);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
