24d74ec09340a2cfc4ee2a6b12495ab91e87a5d1
Noah Too <krivahtoo@gmail.com>
2023-04-12 00:10:19 +0300

    fix allocation type

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **)malloc(height * sizeof(int));
+	arr = (int **)malloc(height * sizeof(int *));
 	for (i = 0; i < height; i++)
 		arr[i] = (int *)malloc(width * sizeof(int));
 
