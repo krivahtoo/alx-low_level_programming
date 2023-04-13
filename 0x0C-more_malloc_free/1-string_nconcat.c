32a221ea3c4bc5af616a5f27f3bae7cd00a21766
Noah Too <krivahtoo@gmail.com>
2023-04-13 22:00:04 +0300

    Task 1. string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: length
+ *
+ * Return: new string pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str = NULL;
+	unsigned int len = 0, i = 0;
+
+	if (s1 != NULL)
+		while (s1[i++] != '\0')
+			len++;
+
+	str = malloc(len + n + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len + n; i++)
+		if (i < len)
+			str[i] = s1[i];
+		else
+			str[i] = s2[i - len];
+	str[i] = '\0';
+
+	return (str);
+}
