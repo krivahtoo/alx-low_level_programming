cd82223998043632060d3e5bc942fe33afcc4784
Noah Too <krivahtoo@gmail.com>
2023-04-14 22:37:22 +0300

    fix: dont use loops

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
 /**
+ * check - check if a number is palindrome
+ *
+ * @s: sring to check
+ * @l: left index
+ * @r: right index
+ *
+ * Return: 1 if true, 0 otherwise
  */
 int check(char *s, int l, int r)
 {
@@ -8,7 +15,21 @@ int check(char *s, int l, int r)
 		return (0);
 	if (l + 1 == r || l == r)
 		return (1);
-	return check(s, l + 1, r - 1);
+	return (check(s, l + 1, r - 1));
+}
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: string to get length
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(++s));
 }
 
 /**
@@ -22,8 +43,7 @@ int is_palindrome(char *s)
 {
 	int i = 0, n = 0;
 
-	while (s[i++] != '\0')
-		n++;
+	n = _strlen(s);
 
-	return check(s, 0, n - 1);
+	return (check(s, 0, n - 1));
 }
