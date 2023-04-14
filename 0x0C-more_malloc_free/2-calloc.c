29d971818f60c9ac2a03285eaef9ab2bcdefadfb
Noah Too <krivahtoo@gmail.com>
2023-04-14 11:50:16 +0300

    Task 2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr = NULL;
+	int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)ptr + 1) = 0;
 
 	return (ptr);
 }
