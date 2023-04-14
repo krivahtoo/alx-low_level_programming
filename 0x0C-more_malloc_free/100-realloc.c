2c48972f7172516fadb67465ed6532483583ccd8
Noah Too <krivahtoo@gmail.com>
2023-04-14 20:28:18 +0300

    fix: Task 4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,6 +19,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size == new_size)
 		return (ptr);
 
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	p = malloc(new_size);
 	if (ptr != NULL)
 	{
