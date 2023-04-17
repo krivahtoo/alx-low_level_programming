ba3b8fe2d8ffcb6aaca80aac9ac3a99b4be4c7e8
Noah Too <krivahtoo@gmail.com>
2023-04-17 22:59:45 +0300

    fix: betty formatting

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,15 @@
 
 #include <stdlib.h>
 
-struct dog {
+/**
+ * struct dog - contains info about a dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
