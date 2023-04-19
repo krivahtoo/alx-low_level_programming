868fbe9decd75d754ccfe3624ff23790334974a6
Noah Too <krivahtoo@gmail.com>
2023-04-20 00:55:52 +0300

    Task 4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: arguments count
+ * @argv: arguments array
+ *
+ * Return: 0 on success, non zero otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int n, i = 0;
+	void *f;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	f = &main;
+	for (i = 0; i < n; i++)
+		printf("%02x ", *(unsigned char *)f++);
+	printf("\n");
+
+	return (0);
+}
