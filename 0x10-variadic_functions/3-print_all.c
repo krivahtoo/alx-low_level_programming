60c63a43e1c3e6bc134517fc0e78cb81357c8f55
Noah Too <krivahtoo@gmail.com>
2023-04-21 10:08:22 +0300

    fix: check if null

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -24,38 +24,39 @@ void print_all(const char * const format, ...)
 	int i = 0, n = 0;
 
 	va_start(ap, format);
-	while (format[i] != '\0')
-	{
-		char *s;
-
-		switch (format[i])
+	if (format != NULL)
+		while (format[i] != '\0')
 		{
-			case 'c':
-				print_sep(n);
-				printf("%c", va_arg(ap, int));
-				n++;
-				break;
-			case 'i':
-				print_sep(n);
-				printf("%d", va_arg(ap, int));
-				n++;
-				break;
-			case 'f':
-				print_sep(n);
-				printf("%f", va_arg(ap, double));
-				n++;
-				break;
-			case 's':
-				print_sep(n);
-				s = va_arg(ap, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s", s);
-				n++;
-				break;
+			char *s;
+
+			switch (format[i])
+			{
+				case 'c':
+					print_sep(n);
+					printf("%c", va_arg(ap, int));
+					n++;
+					break;
+				case 'i':
+					print_sep(n);
+					printf("%d", va_arg(ap, int));
+					n++;
+					break;
+				case 'f':
+					print_sep(n);
+					printf("%f", va_arg(ap, double));
+					n++;
+					break;
+				case 's':
+					print_sep(n);
+					s = va_arg(ap, char *);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s", s);
+					n++;
+					break;
+			}
+			i++;
 		}
-		i++;
-	}
 	va_end(ap);
 
 	printf("\n");
