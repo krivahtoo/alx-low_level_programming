215684c3111416805073e650efa871195f2b57d4
Noah Too <krivahtoo@gmail.com>
2023-04-28 00:21:14 +0300

    Task 5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * startup - prints before the main function is executed.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
