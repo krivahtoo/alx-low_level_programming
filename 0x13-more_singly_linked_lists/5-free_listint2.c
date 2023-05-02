903031d13a18c2f98ebe84842446ca91746501d6
Noah Too <krivahtoo@gmail.com>
2023-05-02 10:39:26 +0300

    fix: task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		while (*head != NULL)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			free(tmp);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
