964bfe44872a36f23e52b8f1f5ea59098b7d1536
Noah Too <krivahtoo@gmail.com>
2023-06-19 09:33:11 +0300

    Task 2. Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,64 @@
+/**
+ * add - adds two numbers
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: integer
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - subtracts two numbers
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: integer
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * mul - multiplies two numbers
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: integer
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * div - divides two numbers
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: integer
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+
+/**
+ * mod - returns the remainder of two numbers
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: integer
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
